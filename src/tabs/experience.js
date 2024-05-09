6d86bb598b609564b20aa2c4633d3ca1aeb9cc4d
mana chins <manatsachinyeruse@gmail.com>
2024-05-09 09:27:42 +0200

    Updated responsibilities

--- src/tabs/experience.js
+++ src/tabs/experience.js
@@ -85,7 +85,8 @@ const Experience = () =>{
                         <h5 className={'text-danger'}>Duties</h5>
                         <ul>
                             <li className={'text-primary'}>Develop, fine tune, and maintain Organization's Systems and Databases</li>
-                            <li className={'text-primary'}>Develop mobile app and additional system functionality.</li>
+                            <li className={'text-primary'}>Develop system to record all operations of the organization in all districts</li>
+                            <li className={'text-primary'}>Develop a react native based mobile application to record all operations to the central system</li>
                             <li className={'text-primary'}>User support and training and data extraction.</li>
                         </ul>
 
@@ -116,6 +117,8 @@ const Experience = () =>{
                             <li className={'text-primary'}>Value Added Services (VAS) Architecture Review</li>
                             <li className={'text-primary'}>Systems Implementation/Development Supervision.</li>
                             <li className={'text-primary'}>Projects management functions </li>
+                            <li className={'text-primary'}>Supervise software development team performance</li>
+                            <li className={'text-primary'}>Minimize risks and implement high standard governance framework for the development team</li>
                         </ul>
                     </Accordion.Body>
                 </Accordion.Item>
@@ -143,6 +146,7 @@ const Experience = () =>{
                             <li className={'text-primary'}>Design, implement, and maintain MySQL and SQL Server databases.</li>
                             <li className={'text-primary'}>Design, implement, deploy, and support funeral policy management software</li>
                             <li className={'text-primary'}>Connect, maintain branch networks across the country.</li>
+                            <li className={'text-primary'}>User Support and training and data extraction</li>
                         </ul>
                     </Accordion.Body>
                 </Accordion.Item>
